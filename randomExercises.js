a617c2dce386c7a350c378493d8c8dab4bc4b9d7
Selyuu <rainierlouis@gmail.com>
2017-08-01 10:43:41 +0100

    updated exercises

--- randomExercises.js
+++ randomExercises.js
@@ -221,4 +221,10 @@ values.forEach(num => {
 })
 
 // Array maximum
-
+const values = [3, 11, 7, 2, 9, 10]
+let result = 0
+for (const num of values) {
+  if (num > result) {
+    result = num
+  }
+}
