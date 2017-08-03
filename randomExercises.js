220ace26d6f362e97f33b4e5773dc4f6b0c61ddd
Selyuu <rainierlouis@gmail.com>
2017-08-03 09:07:32 +0100

    updated exercises

--- randomExercises.js
+++ randomExercises.js
@@ -310,3 +310,27 @@ class Character {
     }
   }
 }
+
+// Dog Class
+class Dog {
+  constructor(name, species, size) {
+    this.name = name
+    this.species = species
+    this.size = size
+  }
+  bark() {
+    if (this.size > 60) {
+      return `Grrr! Grrr!`
+    } else {
+      return `Woof! Woof!`
+    }
+  }
+}
+
+const fang = new Dog("Fang", "boarhound", 75);
+console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+
+
+
+
