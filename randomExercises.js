739ae6b53052fc02459b1bfe18e8ff1cb661d2de
Selyuu <rainierlouis@gmail.com>
2017-08-04 09:43:04 +0100

    updated exercises - reduce/map/filter

--- randomExercises.js
+++ randomExercises.js
@@ -361,3 +361,53 @@ accountArray.forEach(account => account.amount += 1000)
 const numbers = [1, 5, 10, 15];
 
 const totalAmount = numbers.reduce((acc, total) => acc + total, 0) / numbers.length
+
+// Functional Programming
+const movieList = [
+  {
+    title: "Batman",
+    year: 1989,
+    director: "Tim Burton",
+    imdbRating: 7.6
+  },
+  {
+    title: "Batman Returns",
+    year: 1992,
+    director: "Tim Burton",
+    imdbRating: 7.0
+  },
+  {
+    title: "Batman Forever",
+    year: 1995,
+    director: "Joel Schumacher",
+    imdbRating: 5.4
+  },
+  {
+    title: "Batman & Robin",
+    year: 1997,
+    director: "Joel Schumacher",
+    imdbRating: 3.7
+  },
+  {
+    title: "Batman Begins",
+    year: 2005,
+    director: "Christopher Nolan",
+    imdbRating: 8.3
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    director: "Christopher Nolan",
+    imdbRating: 9.0
+  },
+  {
+    title: "The Dark Knight Rises",
+    year: 2012,
+    director: "Christopher Nolan",
+    imdbRating: 8.5
+  }
+];
+
+const moviesBefore2000 = movieList.filter(movie => movie.year < 2000).map(movie => movie.title)
+
+console.log(moviesBefore2000);
